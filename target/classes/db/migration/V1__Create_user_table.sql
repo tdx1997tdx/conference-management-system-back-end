338ee2f4873bc1d7480d8948c763be71216eb371
TangDexuan <798637048@qq.com>
2019-11-21 15:36:42 +0800

    first-commit

--- target/classes/db/migration/V1__Create_user_table.sql
+++
@@ -1,9 +0,0 @@
-create table user
-(
-    user_id int auto_increment primary key not null,
-    name varchar(30) not null,
-    password varchar(30) not null
-
-);
-
-insert into user(name,password) values ("tom","123");
