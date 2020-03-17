f835121d3bb073b9f17db072f36d304817b6471f
TangDexuan <798637048@qq.com>
2020-03-17 16:33:15 +0800

    7天内指定用户会议接口加了hostId

--- src/main/java/com/sustech/conferenceSystem/dto/MeetingSimple.java
+++ src/main/java/com/sustech/conferenceSystem/dto/MeetingSimple.java
@@ -24,6 +24,7 @@ public class MeetingSimple {
     @JSONField(format="yyyy-MM-dd HH:mm:ss")//数据库导出页面时json格式化
     private Date endTime;
     private String hostName;
+    private Integer hostId;
     private String topic;
     private Integer meetingState;
     private Integer userState;
