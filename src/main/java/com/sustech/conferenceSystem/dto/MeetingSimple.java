4ed567bd40cd1aebb1e7da216b6be3a0a15006bc
TangDexuan <798637048@qq.com>
2020-03-20 22:07:04 +0800

    修改 user_meeting7_search 接口，添加返回结果中room信息

--- src/main/java/com/sustech/conferenceSystem/dto/MeetingSimple.java
+++ src/main/java/com/sustech/conferenceSystem/dto/MeetingSimple.java
@@ -17,6 +17,7 @@ public class MeetingSimple {
     private String meetingName;
     private String roomName;
     private Integer roomId;
+    private Room room;
     @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")//页面写入数据库时格式化
     @JSONField(format="yyyy-MM-dd HH:mm:ss")//数据库导出页面时json格式化
     private Date startTime;
